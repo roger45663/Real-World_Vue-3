9e0dd1972ddcac8801b6db361c633167ab764881
unknown <roger45663@gmail.com>
2021-03-26 22:38:35 +0800

    L6-start finihed

--- src/services/EventService.js
+++ src/services/EventService.js
@@ -12,5 +12,8 @@ const apiClient = axios.create({
 export default {
   getEvents() {
     return apiClient.get('/events')
+  },
+  getEventById(id) {
+    return apiClient.get(`/events/${id}`)
   }
 }
